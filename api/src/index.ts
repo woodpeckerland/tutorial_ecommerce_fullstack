18dda062ad41558fdfeb149fcdd3f82908544bb3
woodpeckerland <hans.peter.specht@gmail.com>
2025-07-03 09:07:14 +0200

    json parsing middleware

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,13 +1,28 @@
-import express from "express";
+import express, { json, urlencoded } from "express";
 import productsRoutes from "./routes/products/index";
 
-// Express.js application that sets up a server with basic API endpoints
+// express.js application that sets up a server with basic API endpoints
 const app = express();
+
+// initializing the port for the server
 const port = 3000;
 
-// routers
+// order of middleware is important
+// middleware to parse URL-encoded request bodies
+app.use(urlencoded({ extended: false }));
+
+// middleware to parse JSON request bodies
+app.use(json());
+
+// default route to respond with "Hello World!"
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+// product routes
 app.use("/products", productsRoutes);
 
+// starting the server and listening on the specified port
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
--- api/src/routes/products/productsController.ts
+++ api/src/routes/products/productsController.ts
@@ -14,6 +14,7 @@ export function getProductById(req: Request, res: Response) {
 
 // create
 export function createProduct(req: Request, res: Response) {
+  console.log(req.body);
   res.send("createProduct");
 }
 
