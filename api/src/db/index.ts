a181cbfbedc988c4f40f5f147c489513363e6d30
woodpeckerland <hans.peter.specht@gmail.com>
2025-07-21 10:49:18 +0200

    Changes schema.ts

--- api/src/db/index.ts
+++ api/src/db/index.ts
@@ -1,7 +1,7 @@
 import { drizzle } from "drizzle-orm/node-postgres";
 import { Pool } from "pg";
 
-// pool is a connection pool that manages multiple connections to the database
+// pool that manages multiple connections to the database
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL!,
 });
