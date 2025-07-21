cf4ff79e93a5ff77c3fec53ce22cbdf92ccd7ca8
woodpeckerland <hans.peter.specht@gmail.com>
2025-07-21 16:59:06 +0200

    Fixed dependencies

--- api/src/routes/products/index.ts
+++ api/src/routes/products/index.ts
@@ -10,8 +10,17 @@ import { validateData } from "../../middlewares/validationMiddleware";
 import { z } from "zod";
 
 const createProductSchema = z.object({
-  name: z.string(),
-  price: z.number(),
+  name: z
+    .string()
+    .min(1, "Name must not be empty")
+    .max(255, "Name must be at most 255 characters"),
+  price: z.number().positive("Price must be greater than 0"),
+  description: z.string().optional(),
+  image: z
+    .string()
+    .url("Image URL must be a valid URL")
+    .max(255, "Image URL must be at most 255 characters")
+    .optional(),
 });
 
 // creating API endpoints
@@ -20,6 +29,9 @@ const router = Router();
 
 router.get("/", listProducts);
 router.get("/:id", getProductById);
+// chaining the validation middleware to the createProduct endpoint
+// this will validate the request body against the createProductSchema
+// if the validation fails, it will return a 400 Bad Request response
 router.post("/", validateData(createProductSchema), createProduct);
 router.put("/:id", updateProduct);
 router.delete("/:id", deleteProduct);
