ebc19f7bfffea47b6ca572fe0f837ae99943115e
woodpeckerland <hans.peter.specht@gmail.com>
2025-07-24 17:23:12 +0200

    fix orders

---
+++ api/src/db/orders.ts
@@ -0,0 +1,33 @@
+import {
+  pgTable,
+  integer,
+  timestamp,
+  varchar,
+  doublePrecision,
+} from "drizzle-orm/pg-core";
+import { usersTable } from "./usersSchema.js";
+import { productsTable } from "./productsSchema.js";
+import { z } from "zod";
+
+export const ordersTable = pgTable("orders", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  createdAt: timestamp().notNull().defaultNow(),
+  status: varchar({ length: 50 }).notNull().default("New"),
+  userId: integer()
+    .references(() => usersTable.id)
+    .notNull(),
+});
+
+export const orderItemsTable = pgTable("order_items", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  orderId: integer()
+    .references(() => ordersTable.id)
+    .notNull(),
+  productId: integer()
+    .references(() => productsTable.id)
+    .notNull(),
+  quantity: integer().notNull(),
+  price: doublePrecision().notNull(),
+});
+
+export const insertOrderSchema = z.object({});
