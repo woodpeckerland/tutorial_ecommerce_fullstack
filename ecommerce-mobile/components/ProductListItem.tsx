3c6c082b1939f6969ffeb3e2f633619b692aa0c3
woodpeckerland <hans.peter.specht@gmail.com>
2025-07-30 16:16:55 +0200

    Product item UI using Gluestack

--- ecommerce-mobile/components/ProductListItem.tsx
+++ ecommerce-mobile/components/ProductListItem.tsx
@@ -1,5 +1,54 @@
-import { Text } from "react-native";
+import { Box } from "@/components/ui/box";
+import { Button, ButtonText } from "@/components/ui/button";
+import { Card } from "@/components/ui/card";
+import { Heading } from "@/components/ui/heading";
+import { Image } from "@/components/ui/image";
+import { Text } from "@/components/ui/text";
+import { VStack } from "@/components/ui/vstack";
 
-export default function ProductListItem({ product }) {
-  return <Text style={{ fontSize: 30 }}>{product.name}</Text>;
+type Product = {
+  id: number;
+  name: string;
+  description: string;
+  image: string;
+  price: number;
+};
+
+type Props = {
+  product: Product;
+};
+
+export default function ProductListItem({ product }: Props) {
+  return (
+    <Card className="p-5 rounded-lg max-w-[360px] flex-1">
+      <Image
+        source={{ uri: product.image }}
+        className="mb-6 h-[240px] w-full rounded-md aspect-[4/3]"
+        alt={`${product.name} image`}
+        resizeMode="contain"
+      />
+      <Text className="text-sm font-normal mb-2 text-typography-700">
+        {product.name}
+      </Text>
+      <VStack className="mb-6">
+        <Heading size="md" className="mb-4">
+          ${product.price.toFixed(2)}
+        </Heading>
+        <Text size="sm">{product.description}</Text>
+      </VStack>
+      <Box className="flex-col sm:flex-row">
+        <Button className="px-4 py-2 mr-0 mb-3 sm:mr-3 sm:mb-0 sm:flex-1">
+          <ButtonText size="sm">Add to cart</ButtonText>
+        </Button>
+        <Button
+          variant="outline"
+          className="px-4 py-2 border-outline-300 sm:flex-1"
+        >
+          <ButtonText size="sm" className="text-typography-600">
+            Wishlist
+          </ButtonText>
+        </Button>
+      </Box>
+    </Card>
+  );
 }
