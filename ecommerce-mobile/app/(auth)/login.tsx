6a1ba26302fd173a8fcab89156c397f3ef185298
woodpeckerland <hans.peter.specht@gmail.com>
2025-08-04 15:57:14 +0200

    Login screen UI

---
+++ ecommerce-mobile/app/(auth)/login.tsx
@@ -0,0 +1,65 @@
+import { Button, ButtonText } from "@/components/ui/button";
+import { FormControl } from "@/components/ui/form-control";
+import { Heading } from "@/components/ui/heading";
+import { HStack } from "@/components/ui/hstack";
+import { Input, InputField, InputIcon, InputSlot } from "@/components/ui/input";
+import { Text } from "@/components/ui/text";
+import { VStack } from "@/components/ui/vstack";
+import { EyeIcon, EyeOffIcon } from "lucide-react-native";
+import { useState } from "react";
+import { View } from "react-native";
+
+export default function LoginScreen() {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleState = () => {
+    setShowPassword((showState) => {
+      return !showState;
+    });
+  };
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        alignItems: "center",
+        paddingHorizontal: 16,
+      }}
+    >
+      <FormControl
+        style={{
+          width: "100%",
+          maxWidth: 500,
+        }}
+        className="p-4 border rounded-lg border-outline-300 bg-white m-2"
+      >
+        <VStack space="xl">
+          <Heading className="text-typography-900">Login</Heading>
+          <VStack space="xs">
+            <Text className="text-typography-500">Email</Text>
+            <Input className="min-w-[250px]">
+              <InputField type="text" />
+            </Input>
+          </VStack>
+          <VStack space="xs">
+            <Text className="text-typography-500">Password</Text>
+            <Input className="text-center">
+              <InputField type={showPassword ? "text" : "password"} />
+              <InputSlot className="pr-3" onPress={handleState}>
+                <InputIcon as={showPassword ? EyeIcon : EyeOffIcon} />
+              </InputSlot>
+            </Input>
+          </VStack>
+          <HStack space="sm">
+            <Button className="flex-1" variant="outline" onPress={() => {}}>
+              <ButtonText>Sign up</ButtonText>
+            </Button>
+            <Button className="flex-1" onPress={() => {}}>
+              <ButtonText>Login</ButtonText>
+            </Button>
+          </HStack>
+        </VStack>
+      </FormControl>
+    </View>
+  );
+}
--- ecommerce-mobile/app/_layout.tsx
+++ ecommerce-mobile/app/_layout.tsx
@@ -3,7 +3,7 @@ import { Link, Stack } from "expo-router";
 import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Icon } from "@/components/ui/icon";
-import { ShoppingCart } from "lucide-react-native";
+import { ShoppingCart, User } from "lucide-react-native";
 import { Pressable } from "react-native";
 import { useCart } from "@/store/cartStore";
 import { Text } from "@/components/ui/text";
@@ -22,11 +22,19 @@ export default function RootLayout() {
       <GluestackUIProvider>
         <Stack
           screenOptions={{
+            headerTitleAlign: "center",
             headerRight: () => (
               <Link href={"/cart"} asChild>
-                <Pressable className="flex-row gap-2">
+                <Pressable className="flex-row gap-2 mr-2">
                   <Icon as={ShoppingCart} />
                   <Text>{cartItemsNum}</Text>
+                </Pressable>
+              </Link>
+            ),
+            headerLeft: () => (
+              <Link href={"/login"} asChild>
+                <Pressable className="flex-row gap-2 ml-2">
+                  <Icon as={User} />
                 </Pressable>
               </Link>
             ),
